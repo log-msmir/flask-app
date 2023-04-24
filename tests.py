1d68b85f4a9b1d6e30ab76e811bf7f77a80dfba8
log-msmir <msmir@inbox.ru>
2023-04-24 14:33:43 +0300

    Update testing

--- tests.py
+++ tests.py
@@ -5,17 +5,18 @@ import hello as tested_app
 
 class FlaskAppTests(unittest.TestCase):
     def setUp(self):
-        tested_app.config['TESTING'] - True
+        tested_app.app.config['TESTING'] = True
         self.app = tested_app.app.test_client()
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data.decode('UTF-8'), 'У меня получилось!')
+        self.assertEqual(r.data.decode('utf-8'), 'У меня получилось!')
 
     def test_post(self):
         r = self.app.post('/')
         self.assertEqual(r.status_code, 405)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     unittest.main()
+    
